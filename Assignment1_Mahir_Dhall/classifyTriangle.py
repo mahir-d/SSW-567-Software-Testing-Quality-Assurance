b0b9a71aaafe475eb439d29543c0e44d5a898e1f
mahir-d <mahirdhall4@gmail.com>
2020-09-09 15:28:47 -0400

    Modified assignmnet 1

--- Assignment1_Mahir_Dhall/classifyTriangle.py
+++ Assignment1_Mahir_Dhall/classifyTriangle.py
@@ -6,15 +6,20 @@ How to check for invalid triangle
 Check for floating point precision
 Should i test for good and bad route?
 What should be printed in the text file/report 
+Type of input
+Type of output to equate 
+File distribution
+Design of the funciton 
 """
 
 
-def classify_triangle(a, b, c, f) -> str:
-
+def classify_triangle(a, b, c) -> str:
+    f = open("output.txt", "a")
     f.write(f"Input --> a = {a}, b = {b}, c = {c} \n")
     # Checks for data type of the lengths
     if type(a) != float or type(b) != float or type(c) != float:
         f.write("length of sides should be of type float \n")
+        f.close()
         return "length of sides should be of type float"
 
     # rounds each side to precision of 2
@@ -25,70 +30,76 @@ def classify_triangle(a, b, c, f) -> str:
     # Checks for negative values
     if a < 0 or b < 0 or c < 0:
         f.write("Length of sides cannot be negative \n")
+        f.close()
         return "Length of sides cannot be negative"
 
     # checks for invalid triangle
     if a > b+c or b > a+c or c > a+b:
         f.write("This is not a valid Triangle \n")
+        f.close()
         return "This is not a valid Triangle"
 
     if a == b == c:
         f.write("This is an equilateral Triangle \n")
+        f.close()
         return "This is an equilateral Triangle"
 
     if a == b or a == c or b == c:
         f.write("This is an isosceles Triangle \n")
+        f.close()
         return "This is an isosceles Triangle"
 
     if a*a + b*b == c*c or a*a + c*c == b*b or b*b + c*c == a*a:
         f.write("This is a right angle Triangle \n")
+        f.close()
         return "This is a right angle Triangle"
 
     f.write("This is a scalene Trianlge \n")
+    f.close()
     return "This is a scalene Trianlge"
 
 
 class TriangleTest(unittest.TestCase):
 
     def test_type(self):
-        f = open("test.txt", "a")
-        value = classify_triangle("A", 1.0, 2.0, f)
-        f.close()
+
+        value = classify_triangle("A", 1.0, 2.0)
+        """ ~Test to check for correct input data type~ """
         self.assertEqual(value,
                          "length of sides should be of type float")
 
     def test_negative(self):
-        f = open("test.txt", "a")
-        value = classify_triangle(-1.0, 2.0, 3.0, f)
-        f.close()
+
+        value = classify_triangle(-1.0, 2.0, 3.0)
+        """ ~Test to check for negative values in input~ """
         self.assertEqual(value,
                          "Length of sides cannot be negative")
 
     def test_validity(self):
-        f = open("test.txt", "a")
-        value = classify_triangle(5.0, 2.0, 2.0, f)
-        f.close()
+
+        value = classify_triangle(5.0, 2.0, 2.0)
+        """ ~Test to check for a valid triangle~ """
         self.assertEqual(value,
                          "This is not a valid Triangle")
 
     def test_equilateral(self):
-        f = open("test.txt", "a")
-        value = classify_triangle(5.00002, 5.00004, 5.0007, f)
-        f.close()
+
+        value = classify_triangle(5.00002, 5.00004, 5.0007)
+        """ ~Test to check correct classification of equilateral triangle~ """
         self.assertEqual(value,
                          "This is an equilateral Triangle")
 
     def test_isosceles(self):
-        f = open("test.txt", "a")
-        value = classify_triangle(5.00002, 5.00004, 4.0007, f)
-        f.close()
+
+        value = classify_triangle(5.00002, 5.00004, 4.0007)
+        """ ~Test to check correct classification of isosceles triangle~ """
         self.assertEqual(value,
                          "This is an isosceles Triangle")
 
     def test_right(self):
-        f = open("test.txt", "a")
-        value = classify_triangle(4.0, 5.0, 3.0, f)
-        f.close()
+
+        value = classify_triangle(4.0, 5.0, 3.0)
+        """ ~~Test to check correct classification of right angle triangle~ """
         self.assertEqual(value,
                          "This is a right angle Triangle")
 
